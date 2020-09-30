94b39297cbc631c179dd329103dfed0f91644580
chaim <hmidacheima@gmail.com>
2020-09-30 18:50:26 +0100

    weather

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeatherWidgetMainComponent } from './components/weather-widget-main/weather-widget-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherWidgetMainComponent
+
   ],
   imports: [
     BrowserModule
